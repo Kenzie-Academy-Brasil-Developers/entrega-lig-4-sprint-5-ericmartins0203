0d2c03d122b50d485be940dcbe6865d51e567817
vinicius <vinapro91@gmail.com>
2021-08-09 15:26:17 -0300

    feat:Handle de click

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById('container');
+let verification =  true
 //cria Pedras
 const creatRocks = (player) => {
     const rocks = document.createElement("div")
@@ -22,7 +23,21 @@ const matrix = () => {
     }
 }
 matrix()
+// criar handle de click
+const fazAlgo = (evt) => {
+    let teste = evt.target.parentElement
+    for (let i = 5 ; i>=0 ; i--){
+    if (teste.children[i].length !== 1){
+     let playerX = creatRocks("playerX")
+     teste.children[i].append(playerX)
+     return
+    }
+    
+    console.log(teste.children[i].length)
+    }
+    
 
-
+}
+container.addEventListener("click", fazAlgo)
 
 
