cd50c6e5cd6c41276c4ebea139250f678a234cb3
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-08-09 17:09:31 -0300

    background

--- script.js
+++ script.js
@@ -29,8 +29,6 @@ matrix()
 // criar handle de click
 container.addEventListener("click", (evt) =>{
     let teste = evt.target.parentElement
-    
-    
     for (let i = 5 ; i>=0 ; i--){
         if (teste.children[i].childElementCount !== 1){
             if (verification){
@@ -45,21 +43,21 @@ container.addEventListener("click", (evt) =>{
             }
         }
     }
-    
 });
 
-for (let i= 0;i<container.children.length;i++){
-    for (let j=0; j<container.children[i].children.length;j++){
-        console.log('container.children[i].children[j].children[0].className')
-        if (container.children[i].children[j].childrenElementCount!==0){
-            if (container.children[i].children[j].children[0].className === 'playerX' && 
-            container.children[i].children[j+1].children[0].className === 'playerX' ){
+
+// for (let i= 0;i<container.children.length;i++){
+//     for (let j=0; j<container.children[i].children.length;j++){
+//         console.log('container.children[i].children[j].children[0].className')
+//         if (container.children[i].children[j].childrenElementCount!==0){
+//             if (container.children[i].children[j].children[0].className === 'playerX' && 
+//             container.children[i].children[j+1].children[0].className === 'playerX' ){
                 
-                console.log('parabens')
-            }
-        }
-    }
-}
+//                 console.log('parabens')
+//             }
+//         }
+//     }
+// }
 
 
 // validação
