b2bd974677e495fa1f24704120b3dd78750a6cfb
Eric Esteves Martins <estevesmartinseric@gmail.com>
2021-08-09 17:03:09 -0300

    feat: validação

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 let verification =  true
+
 //cria Pedras
 const creatRocks = (player) => {
     const rocks = document.createElement("div")
@@ -14,6 +15,7 @@ const matrix = () => {
 
         for (let j=0; j < 6; j++){
             let blocks = document.createElement('section');
+            // blocks.setAttribute('data-test',`col ${i} lin ${j}`)
             blocks.id=`block${j}`;
             blocks.className='blocks';
 
@@ -27,6 +29,8 @@ matrix()
 // criar handle de click
 container.addEventListener("click", (evt) =>{
     let teste = evt.target.parentElement
+    
+    
     for (let i = 5 ; i>=0 ; i--){
         if (teste.children[i].childElementCount !== 1){
             if (verification){
@@ -40,8 +44,29 @@ container.addEventListener("click", (evt) =>{
                 return input
             }
         }
-    console.log(teste.children[i].length)
     }
+    
 });
+
+for (let i= 0;i<container.children.length;i++){
+    for (let j=0; j<container.children[i].children.length;j++){
+        console.log('container.children[i].children[j].children[0].className')
+        if (container.children[i].children[j].childrenElementCount!==0){
+            if (container.children[i].children[j].children[0].className === 'playerX' && 
+            container.children[i].children[j+1].children[0].className === 'playerX' ){
+                
+                console.log('parabens')
+            }
+        }
+    }
+}
+
+
+// validação
+
+//horizontal
+//vertical
+//diagonal direita
+//diagonal esquerda
 
 
